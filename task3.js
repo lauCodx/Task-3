08bb6b62c4de1c6ba9ec50c838cda0ac0e36ddd1
lauCodx <lauphix1@gmail.com>
2024-01-22 13:49:21 +0100

    consoled my products

--- task3.js
+++ task3.js
@@ -12,6 +12,10 @@ function Cart() {
         this.items.push(item); // to add a product to the cart
     }
 
+    this.allProducts= function (){
+        return this.items
+    }
+
     // calculates the total price of the items in the cart and empty the cart
     this.check = function() {
         let total = 0;
@@ -23,11 +27,12 @@ function Cart() {
     }
 }
 
-// Usage
+//  we create a new Cart, add some Products to it, and then call check function to calculate the total price.
 let cart = new Cart();
 cart.addItem(new Product("Book", 200));
 cart.addItem(new Product("Cloth", 75));
 cart.addItem(new Product("shoes", 150));
 
+console.log(cart.allProducts())
 console.log(`Total: $${cart.check()}`);
 
